a69ac455698833d667f3aa42d6e1a8e79dab23d6
mey <meydetour@gmail.com>
2023-11-01 19:50:05 +0100

    navbar

--- githubMey/githubMey.js
+++ githubMey/githubMey.js
@@ -7,46 +7,40 @@ let depotContainer = document.querySelector('.profilGithubDepot');
 let amiContainer = document.querySelector('.profilGithubAmi');
 
 
-async function dataProfilGithub(nomGithub) {
-
+async function getData(nomGithub) {
+//parametre : nom d'un profil github (string) Renvoie le fichier json deserialise
     return await fetch('https://api.github.com/users/MeyDetour')
         .then(response => response.json())
         .then(data => {
             return data
         })
 }
-
-async function dicoInformation(nomgithub) {
+async function creerDicoInformation(nomgithub) {
+    //parametre : nom d'un profil github et renvoie un dictionnaire {information : valeur }
     let dico = {}
-    const dataProfil = await dataProfilGithub(nomgithub)
+    const dataProfil = await getData(nomgithub)
     information.forEach((info) => {
         dico[info] = dataProfil[info]
     })
     return dico
 }
 function transformerDate(date){
+    // converti une date str en date formate (fais avec chatgpt)
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
     const formattedDate = new Date(date).toLocaleString('fr-FR', options);
 return formattedDate
 }
 
-dicoInformation('MeyDetour').then(data => {
 
+
+//remplissage des donnees du fichier githubMey.html
+creerDicoInformation('MeyDetour').then(data => {
     const infoMey = data
-    console.log(infoMey)
     document.getElementById('imageMey').src = infoMey['avatar_url']
-    nomContainer.innerHTML += `
-        <div class="d-flex flex-column ">
-        <h1 class="tx-violet1">${infoMey['name']}</h1>
-        <h4><b>Login : </b>${infoMey['login']}</h4>
-        <span><b>Following :</b> <a class="tx-joyeux" href="${infoMey['following_url']}">${infoMey['following']}</a></span>
-        <span><b>Followers :</b> <a class="tx-joyeux" href="${infoMey['followers_url']}">${infoMey['followers']}</a></span>
-        </div>
-       
-        `
+    nomContainer.innerHTML += `<div class="d-flex flex-column "><h1 class="tx-violet1">${infoMey['name']}</h1><h4><b>Login : </b>${infoMey['login']}</h4><span><b>Following :</b> <a class="tx-joyeux" href="${infoMey['following_url']}">${infoMey['following']}</a></span><span><b>Followers :</b> <a class="tx-joyeux" href="${infoMey['followers_url']}">${infoMey['followers']}</a></span></div>`
     bioContainer.innerHTML +=` <div class="d-flex flex-column "><p class="text-bio">${infoMey['bio']}</p></div>`
     idContainer.innerHTML+=` <div class="d-flex flex-column "><span><b>id profil : </b>${infoMey['id']}}</span><a href="${infoMey['html_url']}"  class="tx-joyeux">Regarde mon profil !</a><span><b>Type d'utilisateur :</b> ${infoMey['type']}</span><span><b>Localisation :</b> ${infoMey['location']}</span></div>`
-    dateContainer.innerHTML +=` <div class="d-flex flex-column "><span><b>Date de creation du compte: </b>${transformerDate(infoMey['created_at'])}</span><span><b>Derniere MAJ :</b> ${transformerDate(infoMey['updated_at'])}</span></div>`
+    dateContainer.innerHTML +=` <div class="d-flex flex-column "><span><b>Date de creation du compte: </b>${transformerDate(infoMey['created_at'])}</span><span><b>Derniere profil MAJ :</b> ${transformerDate(infoMey['updated_at'])}</span></div>`
 
 })
 
